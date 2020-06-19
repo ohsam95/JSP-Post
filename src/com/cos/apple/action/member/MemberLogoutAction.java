8d0481b54bc751b6ecea079c9248a6a4f7232d87
ohsam95 <ohsam95@naver.com>
2020-06-19 10:53:28 +0900

    8강 로그아웃 구현 완료

--- src/com/cos/apple/action/member/MemberLogoutAction.java
+++ src/com/cos/apple/action/member/MemberLogoutAction.java
@@ -5,12 +5,16 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.cos.apple.action.Action;
 
 public class MemberLogoutAction implements Action {
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect("member/joinForm.jsp");
+		HttpSession session = request.getSession();
+		session.invalidate();
+		
+		response.sendRedirect("index.jsp");
 	}
 }
